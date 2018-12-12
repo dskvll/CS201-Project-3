33d51dc0544df1c48c3a608063a6d47c16e93a12
D'Armani Nicholson <44146421+dskvll@users.noreply.github.com>
2018-12-12 01:18:46 -0700

    begins setting up error messages
    stores the strings to be printed for error messages

---
+++ project3.s
@@ -0,0 +1,5 @@
+#mips Project 3 recursive base converter
+.data # Data declaration section
+too_long_input: .asciiz "Input is too long."
+out_of_range: .asciiz "Invalid base-27 number." 
+empty_input: .asciiz "Input is empty."
