043eac83a6f44541e3d389460038caecce26a958
D'Armani Nicholson <44146421+dskvll@users.noreply.github.com>
2018-12-12 01:20:28 -0700

    finishes set up and begins reading in user input
    allocates space for user to enter information

--- project3.s
+++ project3.s
@@ -3,3 +3,10 @@
 too_long_input: .asciiz "Input is too long."
 out_of_range: .asciiz "Invalid base-27 number." 
 empty_input: .asciiz "Input is empty."
+user_input: .space 85000
+.text # Assembly language instructions
+main: # Start of code section
+
+# begins getting user input
+
+li $v0, 8   # read string command
